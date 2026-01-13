3f030810d9b720f745fbf359cb44393632fc0082
Archit Jain <146457258+JainArchit16@users.noreply.github.com>
2026-01-13 19:53:30 +0000

    Models added

---
+++ models/Tasks.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "completed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Task || mongoose.model("Task", TaskSchema);
