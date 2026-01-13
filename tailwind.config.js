aa347f993412524b20576a385c388d4f30858eaa
Archit Jain <146457258+JainArchit16@users.noreply.github.com>
2026-01-13 20:03:05 +0000

    Modules Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "grid-pattern":
+          "linear-gradient(to right, #e5e7eb 1px, transparent 1px), linear-gradient(to bottom, #e5e7eb 1px, transparent 1px)",
+      },
+      backgroundSize: {
+        "grid-sm": "24px 24px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
